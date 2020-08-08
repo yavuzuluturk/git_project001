4da235ee1b9ff266ded453c8f28f497109625e78
yavuzuluturk <69388585+yavuzuluturk@users.noreply.github.com>
2020-08-08 21:51:31 +0200

    Update Feature.java
    ı have committed first changes from the remote master branch.

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -4,7 +4,7 @@ public class Feature {
 
 	public static void main(String[] args) {
 		System.out.println("This is my commit from feature branch");
-
+		System.out.println("This is the first commit from remote master brach");
 	}
 
 }
