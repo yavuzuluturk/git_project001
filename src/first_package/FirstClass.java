c5fc48bf352f971537a92e8f8b444a3bb03320ea
yavuzuluturk <yavuzuluturk@gmail.com>
2020-08-08 19:46:47 +0200

    My locak uluturk branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,5 +5,6 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repo");
+		System.out.println("Now I am on my uluturk branch");
 	}
 }
