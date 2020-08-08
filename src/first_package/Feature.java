8dc70086c4a45ecda369cf587a9ea496fa8970dd
yavuzuluturk <69388585+yavuzuluturk@users.noreply.github.com>
2020-08-08 21:59:35 +0200

    Update Feature.java
    The second commit from the remote master branch

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -5,6 +5,7 @@ public class Feature {
 	public static void main(String[] args) {
 		System.out.println("This is my commit from feature branch");
 		System.out.println("This is the first commit from remote master brach");
+		System.out.println("This is the second commit from the remote master brach");
 	}
 
 }
